f0bd7e583e2558618ff1b01c17495050a5890cc3
Emilio Zand <emilio@getbetter.com>
2015-05-04 14:01:03 -0700

    Initial Commit Add SpotifyAPI Add example application

---
+++ src/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spotify_NowPlaying
+{
+    static class Program
+    {
+        /// <summary>
+        /// Simple program to output spotify's nowplaying information to a text file.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
